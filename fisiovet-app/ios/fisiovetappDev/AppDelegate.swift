4ee24620b6e3695495a38859be6b10ef9d3335a4
patamarcelo <patamarcelo@gmail.com>
2025-12-11 15:40:46 -0300

    prebuidl

--- fisiovet-app/ios/fisiovetappDev/AppDelegate.swift
+++ fisiovet-app/ios/fisiovetappDev/AppDelegate.swift
@@ -70,6 +70,12 @@ GMSServices.provideAPIKey("AIzaSyC74WnhqSXYu2RFGeIxKUC46yWAoSFRx9U")
       return false
     }
 // @generated end @react-native-firebase/auth-openURL
+// @generated begin @react-native-firebase/auth-openURL - expo prebuild (DO NOT MODIFY)
+    if url.host?.lowercased() == "firebaseauth" {
+      // invocations for Firebase Auth are handled elsewhere and should not be forwarded to Expo Router
+      return false
+    }
+// @generated end @react-native-firebase/auth-openURL
     return super.application(app, open: url, options: options) || RCTLinkingManager.application(app, open: url, options: options)
   }
 
